5b1466808736fa7929ceec8ced00bef0d6476f09
Ilakiab08 <142892200+Ilakiab08@users.noreply.github.com>
2023-11-08 13:44:34 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,14 @@
+// Get all the buttons with the "grid" class
+const buttons = document.querySelectorAll('#grid');
+
+// Add a click event listener to each button
+buttons.forEach(button => {
+  button.addEventListener('click', () => {
+    // Get the sound file associated with the button
+    const soundFile = button.getAttribute('data-sound');
+
+    // Create an Audio object and play the sound
+    const audio = new Audio(soundFile);
+    audio.play();
+  });
+});
